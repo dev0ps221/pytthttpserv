594c89881e2d3bce53e0401028d85bb528196172
dev0ps221 <seybaxscorpion@gmail.com>
2022-06-26 12:53:51 +0000

    shebang

--- Classes/Connection.py
+++ Classes/Connection.py
@@ -1,17 +1,21 @@
 #!/usr/bin/env python3
+from datetime import datetime
 
 class Connection:
 
     def __init__(self,data,conn_time):
         self.conn_time = conn_time
-        self.data = data
+        self.rawdata = data
+        self.socket = self.rawdata[0]
+        self.address = self.rawdata[1]
         print('new connection with data')
         print(data)
 
 class ConnectionManager:
     connections = []
     def newConnection(self,data):
-       self.connections.append(Connection(data),Date())
+       self.connections.append(Connection(data,datetime.now()))
     def __init__(self,socket):
         self.socket = socket
+
 

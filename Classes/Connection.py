ac18da3fe750fe65a26b6b983d4f56c70c71f683
dev0ps221 <seybaxscorpion@gmail.com>
2022-06-26 12:58:56 +0000

    handle request

--- Classes/Connection.py
+++ Classes/Connection.py
@@ -3,13 +3,32 @@ from datetime import datetime
 
 class Connection:
 
+
+    def handleRequest(self,data):
+
+        print('data')
+
+        return response
+
+    def interact(self):
+        while True:
+            data = self.socket.recv()
+            response = self.handleRequest(data)
+            self.socket.send(response.encode())
+
+
+
     def __init__(self,data,conn_time):
         self.conn_time = conn_time
         self.rawdata = data
         self.socket = self.rawdata[0]
         self.address = self.rawdata[1]
+        self.connecthost = self.address[0]
+        self.connectport = self.address[1]
         print('new connection with data')
-        print(data)
+        print(self.connectport,self.connecthost,self.socket)
+
+
 
 class ConnectionManager:
     connections = []
